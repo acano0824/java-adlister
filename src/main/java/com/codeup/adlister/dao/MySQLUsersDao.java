5e9a7bab37cb8a7da3f36dbc0b3d2de53ae6200c
ashleycano <acano6835@gmail.com>
2018-01-03 13:47:03 -0600

    adding user input exercise

---
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -0,0 +1,73 @@
+package com.codeup.adlister.dao;
+
+import com.codeup.adlister.models.User;
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+
+public class MySQLUsersDao implements Users {
+    private Connection connection;
+
+    public MySQLUsersDao() {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    Config.url,
+                    Config.user,
+                    Config.password
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database!", e);
+        }
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        //write the sql query for findByUsername
+        String sql = "SELECT * FROM users WHERE username = ?";
+
+        try{
+            PreparedStatement stmt = connection.prepareStatement(sql);
+
+            stmt.setString(1, username);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                return new User(
+                        rs.getLong("id"),
+                        rs.getString("username"),
+                        rs.getString("password"),
+                        rs.getString("email")
+                );
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error finding user in the database!");
+        }
+
+    }
+
+    @Override
+    public Long insert(User user) {
+        //write the sql query for the insert
+        String sql = "INSERT INTO users(username, password, email) VALUES (?, ?, ?)";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(
+                    sql, Statement.RETURN_GENERATED_KEYS
+            );
+
+            stmt.setString(1, user.getUsername());
+            stmt.setString(2, user.getPassword());
+            stmt.setString(3, user.getEmail());
+            stmt.executeUpdate();
+
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error inserting user to the database!", e);
+        }
+    }
+}
---
+++ src/main/webapp/WEB-INF/users/register.jsp
@@ -0,0 +1,35 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <jsp:include page="/WEB-INF/partials/head.jsp">
+        <jsp:param name="title" value="Register Here" />
+    </jsp:include>
+</head>
+<body>
+<jsp:include page="/WEB-INF/partials/navbar.jsp" />
+<div class="container">
+    <h1>Welcome! Please Register Below:</h1>
+
+    <form action="/register" method="POST">
+        <div class="form-group">
+            <label for="username">Username</label>
+            <input id="username" name="username" class="form-control" type="text">
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input id="password" name="password" class="form-control" type="text">
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input id="email" name="email" class="form-control" type="text">
+        </div>
+        <div class="form-group">
+            <label for="confirm_password">Confirm Password</label>
+            <input id="confirm_password" name="confirm_password" class="form-control" type="text">
+        </div>
+        <input type="submit" class="btn btn-primary btn-block" value="Register">
+    </form>
+
+</div>
+</body>
+</html>
